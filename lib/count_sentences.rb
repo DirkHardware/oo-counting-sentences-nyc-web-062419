ee57ae1d57be55dc03fd28e159d5c55fb60332ab
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-26 00:17:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -14,20 +14,11 @@ class String
     self.end_with?("!")
   end
 
-#   def count_sentences
-#     self.split(".") && self.split("!") && self.split("?") && self.split("!!")
-#     binding.pry
-#     count = self.count(".") + self.count("!") + self.count("?") +self.count("!!")
-#     binding.pry
-#     count
-#   end
-# end
-
-def count_sentences
-  array = self.split(".") && self.split("!") && self.split("?") && self.split("!!")
-  binding.pry
-  count = array.count(".") + array.count("!") + array.count("?") + array.count("!!")
-  binding.pry
-  count
-end
+  def count_sentences
+    self.split(".") && self.split("!") && self.split("?") && self.split("!!")
+    binding.pry
+    count = self.count(".") + self.count("!") + self.count("?") +self.count("!!")
+    binding.pry
+    count
+  end
 end
